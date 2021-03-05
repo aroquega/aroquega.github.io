af4deea664dbb70494f0cad7d193284abc66e819
Hristo <aroquega@unsa.edu.pe>
2021-03-04 23:26:51 -0500

    create skeleton for the portofolio page (index)

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,30 +2,26 @@ import React from "react"
 
 import Layout from '../components/layout'
 import Header from '../components/header'
-import CVSection from '../components/cvsection'
+import Card from '../components/card'
 
-const IndexPage = () => (
+const IndexPage = ({location}) => (
   <Layout>
-    <Header/>
-    <CVSection
-      title="About me"
-      content={
-        `I am a software web developer. I live in Arequipa, Peru I work with
-        javascript, react, and python.`
-      }
-    />
-    <CVSection
-      title="Education"
-      content={
-        `I'm currently studying at UNSA University.`
-      }
-    />
-    <CVSection
-      title="Languages"
-      content={
-        `I am a native spanish speaker, and I'm `
-      }
-    />
+    <Header location={location}></Header>
+    <div className="bg-mountains-md bg-cover bg-center bg-fixed p-5 h-56">
+      <div className="bg-cyanprocess bg-opacity-75 p-5 w-full md:w-2/4 shadow-md">
+        <p>
+          I'm a backend web developer that focuses on creating high quality code.
+          I'm always learning and trying out new technologies and areas.
+        </p>
+      </div>
+    </div>
+    <article className="p-5">
+      <h2 className="text-3xl">Projects</h2>
+      <div class="flex flex-wrap">
+        <Card />
+        <Card />
+      </div>
+    </article>
   </Layout>
 )
 
