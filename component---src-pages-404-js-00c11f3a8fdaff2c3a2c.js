b9f538497a02950446814ffc5e2e23eedc62d8ec
Hristo <aroquega@unsa.edu.pe>
2021-03-05 00:45:38 -0500

    Updates

---
+++ component---src-pages-404-js-00c11f3a8fdaff2c3a2c.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{w2l6:function(n,e,t){"use strict";t.r(e);var o=t("q1tI"),u=t.n(o);e.default=function(){return u.a.createElement("h1",null,"Page not found")}}}]);
+//# sourceMappingURL=component---src-pages-404-js-00c11f3a8fdaff2c3a2c.js.map
