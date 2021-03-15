76e322ed0b2d8c78684b8bfd0a19f32f1ba74200
Hristo <aroquega@unsa.edu.pe>
2021-03-15 00:18:54 -0500

    add project data to test components

--- src/components/card.js
+++ src/components/card.js
@@ -1,24 +1,24 @@
 import React from 'react'
 import {Link} from '../components/controls'
 
-function Card() {
+function Card({project}) {
   return (
     <article
       className="
-        w-full h-60 my-4">
+        w-full h-32 my-4">
       <header className="text-lg">
-        <h3>Título del Proyecto</h3>  
+        <h3>{project.title}</h3>  
       </header>    
       <img/>
       <p className="mt-2 mb-4 text-sm">
-        Descripción del Proyecto
+        {project.description}
       </p>
       <div className="flex">
         <Link>
           ver en detalle
         </Link>
         <Link type='text'>
-          ir a github
+          ver código fuente
         </Link>
         <Link type='text'>
           ver demo
---
+++ src/data/projects.js
@@ -0,0 +1,31 @@
+export default [
+    {
+        id: 1,
+        title: "FunChores",
+        description: "In Development",
+        technologies: [
+            'asp.net',
+            'c#',
+            'react',
+            'javascript',
+        ],
+        links: {
+            source: '',
+            live: '',
+        }
+    },
+    {
+        id: 2,
+        title: "Buggtracker",
+        description: "In Development",
+        technologies: [
+            'asp.net',
+            'c#',
+            'blazor',
+        ],
+        links: {
+            source: '',
+            live: '',
+        }
+    },
+]
