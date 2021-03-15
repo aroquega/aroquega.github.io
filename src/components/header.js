be407679e92efd1644215732168b781077aa7e07
Hristo <aroquega@unsa.edu.pe>
2021-03-14 23:22:49 -0500

    add more links to the header

--- src/components/header.js
+++ src/components/header.js
@@ -17,35 +17,24 @@ function Header({ location }) {
   //   }
   // })
 
-  const actionButton = getActionButton(location.pathname)
   return (
     <header
       className={`
         h-16 w-full px-6
         flex items-center justify-between
-        bg-xiketic text-eggshell shadow-md fixed
+        bg-xiketic text-eggshell shadow-md
+        sticky top-0
         bg-opacity-95`}>
       <h1>
         Hristo Roque
       </h1>
       <nav>
-        <Link
-          to={actionButton.to}
-          className="
-            block text-xiketic text-xs text-center font-bold uppercase w-32
-            bg-cyanprocess p-2 rounded shadow-md">
-          {actionButton.name}
-        </Link>
+        <a className="m-2">About me</a>
+        <a className="m-2">Projects</a>
+        <a className="m-2">Resume</a>
       </nav>
     </header>
   )
-}
-
-function getActionButton(pathname) {
-  if (pathname === '/cv') {
-    return { to: '/', name: 'ver portafolio' }
-  }
-  return { to: '/cv', name: 'ver cv' }
 }
 
 export default Header
