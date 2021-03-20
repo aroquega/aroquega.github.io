6ab785f20a5a33e7c8dd2563010b2f871a13a88c
Hristo Roque <aroquega@unsa.edu.pe>
2021-03-20 11:51:58 -0500

    add a drawer when mobile

--- src/components/header.js
+++ src/components/header.js
@@ -1,39 +1,76 @@
-import React, { useState, useEffect } from 'react'
-import { Link } from 'gatsby'
+import React, { useState } from 'react'
 
 function Header({ location }) {
-  // const [scrollPos, setScrollPos] = useState(window.pageYOffset)
-  // const [active, setActive] = useState(false)
-
-  // useEffect(() => {
-  //   window.addEventListener('scroll', () => {
-  //     const currentScrollPos = window.pageYOffset
-  //     console.log(`current: ${currentScrollPos}. prev: ${scrollPos}`)
-  //     setActive(scrollPos > currentScrollPos)
-  //     setScrollPos(currentScrollPos)
-  //   })
-  //   return () => {
-  //     window.removeEventListener('scroll')
-  //   }
-  // })
+  const [isMenuOpen, setMenuOpen] = useState(false);
 
   return (
     <header
       className={`
         h-16 w-full px-6
-        flex items-center justify-between
+        flex items-center justify-start md:justify-between
         bg-xiketic text-white shadow-md
         sticky top-0
-        bg-opacity-95`}>
-      <h1>
-        Hristo Roque
-      </h1>
-      <nav>
-        <a className="m-2">About me</a>
-        <a className="m-2">Projects</a>
-        <a className="m-2">Resume</a>
+        bg-opacity-95
+        `}>
+      <div className="flex items-center">
+        <button className="mr-2" onClick={() => setMenuOpen(true)}>
+          <span className="material-icons block md:hidden">
+            menu
+          </span>
+        </button>
+        <h1>
+          Hristo Roque
+        </h1>
+      </div>
+      <nav className="hidden md:block">
+        <ul className="flex">
+          <li>
+            <a href="/about" className="py-2 px-4 block flex items-center">
+              <span className="material-icons mr-2">person</span> About me
+            </a>
+          </li>
+          <li>
+            <a href="/projects" className="py-2 px-4 block flex items-center">
+              <span className="material-icons mr-2">folder</span> Projects
+            </a>
+          </li>
+          <li>
+            <a href="/resume" className="py-2 px-4 block flex items-center">
+              <span className="material-icons mr-2">description</span> Resume
+            </a>
+          </li>
+        </ul>
       </nav>
-    </header>
+      <nav
+        className={
+          `md:hidden bg-xiketic fixed top-0 bottom-0 left-0 w-full shadow-lg
+          transform-gpu ${isMenuOpen ? 'translate-x-0' : '-translate-x-full'}
+          transition duration-500 ease-in-out
+          `}>
+        <div className="flex justify-end p-2 bg-primaryLight h-16">
+          <button onClick={() => setMenuOpen(false)}>
+            <span className="material-icons">close</span>
+          </button>
+        </div>
+        <ul>
+          <li>
+            <a href="/about" className="py-2 px-4 block flex items-center">
+              <span className="material-icons mr-2">person</span> About me
+            </a>
+          </li>
+          <li>
+            <a href="/projects" className="py-2 px-4 block flex items-center">
+              <span className="material-icons mr-2">folder</span> Projects
+            </a>
+          </li>
+          <li>
+            <a href="/resume" className="py-2 px-4 block flex items-center">
+              <span className="material-icons mr-2">description</span> Resume
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header >
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,11 @@ module.exports = {
     extend: {
       backgroundImage: theme => ({
         'mountains-md': "url('../images/background-md.webp')",
-      })
+      }),
     },
     colors: {
       xiketic: '#07020d',
+      primaryLight: '#0b0414',
       cyanprocess: '#0371a0',
       eggshell: '#f1e9db',
       grullo: '#a39b8b',
